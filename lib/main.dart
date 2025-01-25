225c5ffecad7e03ab61ed61d7c0a067adf48c8e2
Mohamed Sharaf <mohamed.a.sharaf.maro@gmail.com>
2025-01-26 00:32:20 +0200

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello, World!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+class GradiantContainer extends StatelessWidget {
+  const GradiantContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return 
+
+  }
+}
